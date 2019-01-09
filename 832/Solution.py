3fee16c2ad0ea09bdf2374954035febe35096794
hhe0 <deng2296917779@163.com>
2019-01-09 17:33:38 +0800

    # 832

--- 832/Solution.py
+++ 832/Solution.py
@@ -4,3 +4,18 @@ class Solution:
         :type A: List[List[int]]
         :rtype: List[List[int]]
         """
+        res_list = []
+        for lists in A:
+            tmp_list = []
+            for list in lists[::-1]:
+                if list == 1:
+                    tmp_list.append(0)
+                else:
+                    tmp_list.append(1)
+            res_list.append(tmp_list)
+
+        return res_list
+
+
+solution = Solution()
+solution.flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]])
