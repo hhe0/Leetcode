a7ff8b8b214f66c1c69ebcef7c9afdc123689ee2
hhe <hhe_minc@126.com>
2019-01-09 16:26:26 +0800

    # 344

---
+++ 344/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
+
+
+solution = Solution()
+print(solution.reverseString('hhe'))
