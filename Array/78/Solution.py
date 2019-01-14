c34805acba3074b72760abbe415aee101dd8567b
hhe0 <deng2296917779@163.com>
2019-01-14 10:44:07 +0800

    # 78

--- Array/78/Solution.py
+++ Array/78/Solution.py
@@ -4,7 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        res = [[]]
+
+        for i in range(len(nums)):
+            for j in range(len(res)):
+                # 在原有集合的基础上加上一个元素
+                res.append(res[j] + [nums[i]])
+
+        return res
 
 
 solution = Solution()
-solution.subsets([1, 2, 3])
+print(solution.subsets([1, 2, 3]))
