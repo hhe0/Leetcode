51a371533f66e4a7569c83b16abb61db823a1fb2
hhe0 <deng2296917779@163.com>
2019-01-14 10:33:35 +0800

    # 867

---
+++ Array/78/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+
+solution = Solution()
+solution.subsets([1, 2, 3])
