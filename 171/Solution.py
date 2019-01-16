7700abe2b08eec73488c92116c8d19501543461e
hhe0 <deng2296917779@163.com>
2019-01-16 11:42:35 +0800

    # 171

---
+++ 171/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+        for ch in s:
+            print(ch)
+            num = num * 26 + (ord(ch) - ord('A') + 1)
+
+        return num
+
+s = 'ZY'
+solution = Solution()
+solution.titleToNumber(s)
+
