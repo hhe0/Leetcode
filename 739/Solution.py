fd801fd8bb52f688b3e0b37328c875d819c35784
hhe0 <deng2296917779@163.com>
2019-01-22 15:27:53 +0800

    #739

--- 739/Solution.py
+++ 739/Solution.py
@@ -4,22 +4,21 @@ class Solution:
         :type T: List[int]
         :rtype: List[int]
         """
-        res = []
-        for i in range(0, len(T)):
-            flag = False
-            num = 1
-            for j in range(i+1, len(T)):
-                if T[j] > T[i]:
-                    flag = True
-                    res.append(num)
-                    break
-                num += 1
-            if not flag:
-                res.append(0)
+        stack = []
+        t_length = len(T)
+        res = [0 for _ in range(t_length)]
+
+        for key, value in enumerate(T):
+            if stack:
+                while stack and T[stack[-1]] < value:
+                    res[stack[-1]] = key - stack[-1]
+                    stack.pop()
+            stack.append(key)
 
         return res
 
 
 T = [73, 74, 75, 71, 69, 72, 76, 73]
 solution = Solution()
-solution.dailyTemperatures(T)
+res = solution.dailyTemperatures(T)
+print(res)
