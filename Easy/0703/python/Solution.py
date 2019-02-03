bba33d6c06066feb42530354f9f7a6f80da1fdf1
hhe0 <deng2296917779@163.com>
2019-02-03 14:30:58 +0800

    # 0703

---
+++ Easy/0703/python/Solution.py
@@ -0,0 +1,33 @@
+class KthLargest:
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.k = k
+        self.nums = nums
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        self.nums.append(val)
+        self.nums.sort(reverse=True)
+
+        return self.nums[self.k-1]
+
+
+# Your KthLargest object will be instantiated and called as such:
+
+
+nums = [4, 5, 8, 2]
+k = 3
+obj = KthLargest(k, nums)
+print(obj.add(3))
+print(obj.add(5))
+print(obj.add(10))
+print(obj.add(9))
+print(obj.add(4))
+
